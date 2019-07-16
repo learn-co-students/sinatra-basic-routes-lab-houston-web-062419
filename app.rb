1c7aee91118c619796c46949ac92281302f5f3bf
Aaron Briscoe <gmail2008@gmail.com>
2019-07-16 16:49:04 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    
+    get '/' do 
+        "Hello, WOrld!"
+    end
+
+    get '/name' do 
+        "My name is Aaron"
+    end
+
+
+    get '/hometown' do
+        "My hometown is H-Town!"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'Happy Birthday'"
+    end
 end
